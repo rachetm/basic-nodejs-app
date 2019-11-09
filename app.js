87aa546ccc09c448599f453a01194a0efc011924
Rachet Mudnur <rmudnur@gmail.com>
2019-11-09 21:44:28 +0530

    Changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ db.on('error', (err) => console.log(err.message));
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
